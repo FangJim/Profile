8cd93e1b6a537180479a89be7f22d35830683a48
Jim Fang <jmjmjim1101@gmail.com>
2022-01-02 18:51:59 +0800

    Finish Home

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+//Header
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(element => element.addEventListener('click', linkAction))
+
+//
