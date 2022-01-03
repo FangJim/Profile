da72887ab816844ba044536fa9cb0daad2f1e65a
Jim Fang <jmjmjim1101@gmail.com>
2022-01-03 13:59:50 +0800

    Finish Skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 //Header
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
@@ -24,4 +23,30 @@ function linkAction(){
 }
 navLink.forEach(element => element.addEventListener('click', linkAction))
 
-//
+//skill anime
+const skillsContent = document.querySelectorAll('.skills_content')
+const skillsHeader = document.querySelectorAll('.skills_header')
+const skillsPercentages = document.querySelectorAll('.skills_percentage')
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsPercentages.forEach((bar)=>{
+            bar.style.animation = ''
+        })
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className =  'skills_content skills_open'
+        skillsPercentages.forEach((bar)=>{
+            if(bar.parentNode.parentNode.parentNode.parentNode.className==='skills_content skills_open')
+                bar.style.animation = 'percentage 3s ease-in-out forwards'
+            else{
+                bar.style.animation = ''
+            }
+        })
+    }
+}
+skillsHeader.forEach((element)=>{
+    element.addEventListener('click',toggleSkills);
+})
