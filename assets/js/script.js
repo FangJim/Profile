9f82c3b2a173a5ba0e55912ad3a31e4a49ef8db4
Jim Fang <jmjmjim1101@gmail.com>
2022-01-06 08:29:20 +0800

    Debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,23 @@ function scrollHeader(){
         header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+//scroll anime
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
